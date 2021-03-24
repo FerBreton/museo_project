fbd6353fd89007c3ee96b87f250587e7ef8a5fc0
FerBreton <fernanda.bretonq@gmail.com>
2021-03-24 13:19:34 -0600

    timestamp auto

--- museo/visitas/models.py
+++ museo/visitas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Visita(models.Model):
+    email = models.EmailField()
+    timestamp_in = models.DateTimeField(auto_now_add=True)
+    timestamp_out = models.DateTimeField(auto_now=True)
+    comment = models.CharField(max_length=140)
