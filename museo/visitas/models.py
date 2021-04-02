f65b8b040b2dcb2b975f63ba7bfaaa04bd9045c7
Fernanda Bretón <188108@iberopuebla.mx>
2021-04-02 17:07:04 -0600

    [UPDATE] models correction

--- museo/visitas/models.py
+++ museo/visitas/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Visita(models.Model):
     email = models.EmailField()
     timestamp_in = models.DateTimeField(auto_now_add=True)
-    timestamp_out = models.DateTimeField(auto_now=True)
-    comment = models.CharField(max_length=140)
+    timestamp_out = models.DateTimeField(blank=True, null=True)
+    comment = models.CharField(max_length=140, blank=True)
