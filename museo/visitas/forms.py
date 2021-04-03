e3c02676a2578654270fe4e02746c03bfc3d0fc3
Fernanda Bretón <188108@iberopuebla.mx>
2021-04-03 14:06:22 -0600

    Visitas Form

---
+++ museo/visitas/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import *
+
+class VisitaForm(forms.ModelForm):
+    class Meta:
+        model = Visita
+        fields = '__all__'
+        exclude = ['timestamp_out','comment']
--- museo/visitas/views.py
+++ museo/visitas/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views import View
+from .forms import *
 # Create your views here.
 
-def home(request):
-    return render(request, "index.html", {})
+#def home(request):
+ #   return render(request, "index.html", {})
+
+class Home(View):
+    def get(self, request):
+        form = VisitaForm()
+        context = {'form': form}
+        return render(request, 'index.html', context)
+    
+    def post(self, request):
+        form = VisitaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        else:
+            context = {'form': form}
+            return render(request, 'index.html', context)
